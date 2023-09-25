a2b5a887aa6111edc1f12ca285debc64de65c811
neeraj verma <neerajverma@neerajs-MacBook-Air.local>
2023-09-25 21:37:15 +0530

    installed

---
+++ src/component/Square.jsx
@@ -0,0 +1,9 @@
+const Square = ({ value }) => {
+  //   console.log(props.value);
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
+};
+export default Square;
